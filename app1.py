3844a7a47ac2596a2cb7849b7a3caee8d11ea3e6
Sudhanshu Wani <wanisudhanshu@gmail.com>
2023-09-08 10:55:23 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -182,7 +182,7 @@ if st.session_state.advice_generated:
 
         #{st.session_state.advice_counter}
 
-        st.write(f"FinSafe used: 324 times")
+        st.write(f"FinSafe used: 347 times")
 
 
 # Footer
